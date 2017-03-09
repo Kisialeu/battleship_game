0657b5b58a9127fc4e64c4a46745ac4914d51877
Kisialeu <kisialeu@gmail.com>
2017-03-09 22:45:05 +0300

    Update battleship.py
    Add counter for missing exception
    Add break for a win

--- battleship.py
+++ battleship.py
@@ -34,6 +34,7 @@ for turn in range(4):
     
     if guess_row == ship_row and guess_col == ship_col:
         print "Congratulations! You sunk my battleship!"
+        break
     else:
         if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
             print "Oops, that's not even in the ocean."
@@ -42,5 +43,9 @@ for turn in range(4):
         else:
             print "You missed my battleship!"
             board[guess_row][guess_col] = "X"
-        print "Turn", turn+1
+            print "Turn", turn+1
         print_board(board)
+        
+        if turn == 3:
+            print "Game Over"
+        
